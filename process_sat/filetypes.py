70d950ac475c746622f377d8136a2491d88d5d55
Jacob Oberman <oberman@wisc.edu>
2012-04-13 13:42:59 -0500

    Put in file for filetypes

---
+++ process_sat/filetypes.py
@@ -0,0 +1,16 @@
+'''
+List of supported filetypes and associated parameters
+
+Each class in this file has, at minimum, an attribute "parser"
+that specifies the associated parser for that class.  Each class
+may also contain any other variables that will be used for
+output function attributes if the output function accepts those
+attributes
+
+In some cases, output function attributes may consist of lists
+associated with field names for that particular filetype.  In 
+these cases, the variables for these attributes will be 
+dictionaries keyed off the field name strings.
+'''
+
+
