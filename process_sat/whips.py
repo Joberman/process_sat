89a7496985e54820240f59124956cc8ecdc56944
Jacob Oberman <oberman@wisc.edu>
2012-07-16 15:23:46 -0500

    Updated version number in whips.py

--- process_sat/whips.py
+++ process_sat/whips.py
@@ -28,7 +28,7 @@ from process_sat import filetypes
 '''
 VERSION NUMBER
 '''
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 class NeedToParseInFileException(Exception):
     '''exception class for signaling the need to parse input file'''
